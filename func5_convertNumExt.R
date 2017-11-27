41c08c229cfac79c1cc8bc8203065bc81797c4d0
Hindol Rakshit <hindol@pin2p.in>
2017-11-27 11:59:24 +0100

    Removes single character extensions and converts to numeric.

---
+++ func5_convertNumExt.R
@@ -0,0 +1,42 @@
+# Broad scale wrangling functions
+# Targeted at solving multiple (similar) problems at the same time
+# Operated on datsets in the form of static .csv files
+
+# Prerequisite: Singular uploaed dataframe (works in principle for data.tables as well)
+
+
+# ...........................................................................
+##### Issue 5: Remove single character extension and convert to numeric #####
+# ...........................................................................
+
+convertNumExt = function(d, header, ext) {
+  
+  # This function removes character extensions and converts header to numeric
+  
+  if (header %in% colnames(d)) {
+    
+    if (as.character(is.numeric(d[ , header])) == "FALSE") {
+      
+      d[ , header] = gsub(ext, "\\1", d[ , header])
+      
+      d[ , header] = as.numeric(paste(d[ , header]))
+      
+    } else {
+      
+      # EXCEPTION HANDLING:
+      
+      print("HEADER NOT PROCESSED. Given header is already numerical.")
+      
+    }
+    
+  } else {
+    
+    # EXCEPTION HANDLING:
+    
+    print("HEADER NOT PROCESSED. Given header not available in the dataset.")
+    
+  }
+  
+  return(d)
+  
+}
