46509fe3f1e7603a96a2e6788bb98390b8836e44
Hindol Rakshit <hindol@pin2p.in>
2017-11-27 12:01:11 +0100

    Removes extensions AFTER a specified character.

---
+++ func7_removeAfter.R
@@ -0,0 +1,35 @@
+# Broad scale wrangling functions
+# Targeted at solving multiple (similar) problems at the same time
+# Operated on datsets in the form of static .csv files
+
+# Prerequisite: Singular uploaed dataframe (works in principle for data.tables as well)
+
+
+# ...................................................
+##### Issue 7: Remove everything after a symbol #####
+# ...................................................
+
+removeAfter = function(d, header, separator) {
+  
+  # This function removes everything after a designated symbol
+  
+  if (header %in% colnames(d)) {
+    
+    sep_removeAfter = paste0(separator, ".*")
+    
+    d[ , header] = gsub(sep_removeAfter, "", d[ , header])
+    
+    d[ , header] = factor(d[ , header])
+    
+  } else {
+    
+    # EXCEPTION HANDLING:
+    
+    print("HEADER NOT PROCESSED. Given header not available in the dataset.")
+    
+    
+  }
+  
+  return(d)
+  
+}
