e27a5a144b087484cafe43c66a844f65b0fb0f6d
Hindol Rakshit <hindol@pin2p.in>
2017-12-01 14:16:55 +0100

    Add files via upload

---
+++ raw functions/func1_cleanBlanck.R
@@ -0,0 +1,27 @@
+# Broad scale wrangling functions
+# Targeted at solving multiple (similar) problems at the same time
+# Operated on datsets in the form of static .csv files
+
+# Prerequisite: Singular uploaed dataframe (works in principle for data.tables as well)
+
+# ......................................
+##### Issue 1: Clean blanck spaces #####
+# ......................................
+
+
+cleanBlanck = function(d) {
+  
+  # This function replaces blanck spaces (missing data) in our dataframes with NA
+  
+  for (i in 1:ncol(d)) {
+    
+    d[,i][which(d[,i] == '')] = NA
+    d[,i][which(d[,i] == ' ')] = NA
+    d[,i][which(d[,i] == '-')] = NA
+    d[,i][which(d[,i] == '*')] = NA
+    
+  }
+  
+  return(d)
+  
+}
