f26ffa32fa6fe018a7392bfae51916b3ed89d668
Hindol Rakshit <hindol@sourcingbot.com>
2017-12-07 13:33:02 +0100

    Prerun + run for unit testing

--- unit testing/unit_test_prerun.R
+++ unit testing/unit_test_prerun.R
@@ -1,5 +1,5 @@
 # Unit testing normalization functions
-# Resource: https://bitbucket.org/account/user/srcbot/projects/DW
+# Resource: https://github.com/sourcingbot/Wrangling-Functions-in-R
 
 
 # ..................................
@@ -15,22 +15,23 @@ rm(list = ls())
 
 library(dfwrangle)
 
-# if ("RCurl" %in% installed.packages()[,"Package"] == FALSE) {
+if ("RCurl" %in% installed.packages()[,"Package"] == FALSE) {
   
-  # install.packages("RCurl")
+  install.packages("RCurl")
   
-# }
+}
 
-# library(RCurl)
+library(RCurl)
 library(testthat)
 
 # .............................
 ##### Load sample dataset #####
 # .............................
 
-# Download sample data from https://bitbucket.org/srcbot/wrangling-functions-in-r/src/95d13a5131f8/unit%20testing/?at=master
+# Download sample data from https://github.com/sourcingbot/Wrangling-Functions-in-R/blob/master/unit%20testing/msr_sample_data.csv
+# Or source it directly from the raw GitHub link, usng the "RCurl" library as follows:
 
-d = read.csv("C:/Pin2Pin/NewResearchStuff/Data Normalization/unit testing/msr_sample_data.csv", encoding = "UTF-8")
+d = read.csv(text = getURL("https://raw.githubusercontent.com/sourcingbot/Wrangling-Functions-in-R/master/unit%20testing/msr_sample_data.csv?token=AVmzxbKqs_cRaia_-rwRhW3cnqPcXQBIks5aMm6iwA%3D%3D"), header = T, encoding = "UTF-8")
 
 
 # ..........................................
@@ -45,25 +46,3 @@ setwd("C:/Pin2Pin/NewResearchStuff/Data Normalization/unit testing/tests")
 # .......................
 
 test_dir(".")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
