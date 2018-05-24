c370dac2eefbf9ab7de46691654c33a8c142b631
Ereli Eran <ereli@sourcingbot.com>
2018-05-24 14:17:15 -0400

    handling new unknown categories and distributors

--- functions.py
+++ functions.py
@@ -12,8 +12,7 @@ import normalization as n
 import common as c
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(CURRENT_DIR, 'mapping.yml'), 'r') as f:
-  MAPPING = yaml.load(f)
-
+    MAPPING = yaml.load(f)
 
 
 def deep_set(part, value, keys):
@@ -24,11 +23,11 @@ def deep_set(part, value, keys):
 
 
 def adjust_structure(part: dict, source: str, ts: int):
-  
+
     part = defaultdict(dict, part)
     # fix mfr before everything
     if 'mfr' in part:
-        #mfr = c.get_alias(part['mfr'])
+        # mfr = c.get_alias(part['mfr'])
         mfr = c.get_mfr_mapping(part['mfr'])
         part['mfr'] = mfr
     # keep desciprtion
@@ -41,7 +40,7 @@ def adjust_structure(part: dict, source: str, ts: int):
         raw_categories = deepcopy(part['categories'])
         part['categories_raw'] = {}
         part['categories_raw'][source] = raw_categories
-		# save raw mpn before normalizing
+        # save raw mpn before normalizing
     if 'mpn' in part:
         raw_mpn = deepcopy(part['mpn'])
         part['mpn_raw'] = {}
@@ -52,51 +51,52 @@ def adjust_structure(part: dict, source: str, ts: int):
     part.pop('packagecase', None)
     part.pop('minimum_quantity', None)
     for key in list(part):
-    # apply norm
+        # apply norm
         if key in MAPPING[source]:
             try:
                 # attempt to apply functions
                 if 'actions' in MAPPING[source][key]:
                     functions = MAPPING[source][key]['actions']
                     for func in functions:
-                    # print(f,key)
+                        # print(f,key)
                         new_val = eval(
                             "n." + func + "({})".format("part['" + key + "']"))
                     part[key] = new_val
             except Exception as error:
-                print('Caught this error: '+ repr(error)+' during processing of function '
-                      +func
-                      +" and key: "
-                      +repr(key)
-                      +" and value: "
+                print('Caught this error: ' + repr(error) + ' during processing of function '
+                      + func
+                      + " and key: "
+                      + repr(key)
+                      + " and value: "
                       + repr(part[key])
-                     )
+                      )
 
             # check for new keys name
             if isinstance(MAPPING[source][key]['output_key'], list) and isinstance(part[key], tuple):
-                t_res = dict(zip(MAPPING[source][key]['output_key'], part[key]))
+                t_res = dict(
+                    zip(MAPPING[source][key]['output_key'], part[key]))
                 for k in list(t_res.keys()):
                     if part[key] != MAPPING[source][key]['output_key']:
                         part[k] = t_res[k]
                         part.pop(key)
                     else:
                         part[k] = t_res[k]
-            elif  isinstance(MAPPING[source][key]['output_key'], str) and isinstance(part[key], tuple):
+            elif isinstance(MAPPING[source][key]['output_key'], str) and isinstance(part[key], tuple):
                 print("handling single output key")
                 output_key = MAPPING[source][key]['output_key']
                 part[output_key] = part.pop(key)
-            elif  isinstance(MAPPING[source][key]['output_key'], str):
+            elif isinstance(MAPPING[source][key]['output_key'], str):
                 output_key = MAPPING[source][key]['output_key']
                 part[output_key] = part.pop(key)
             else:
                 new_keys = MAPPING[source][key]['output_key']
-                print("couldn't assign new key. for old key {0}, new_keys: {2}, data: {1}".format(str(key), str(part[key]), str(new_keys)))
-
+                print("couldn't assign new key. for old key {0}, new_keys: {2}, data: {1}".format(
+                    str(key), str(part[key]), str(new_keys)))
 
         else:
             # call missing-mapping queue with the source, categories and
             # missing mapping key.
-            #print("missing mapping")
+            # print("missing mapping")
             c.send_msg(dumps(
                 {"source": source, "categories": part['categories'], "key": key}))
             # we are going to continue in order to prevent writing json that's
@@ -110,8 +110,8 @@ def adjust_structure(part: dict, source: str, ts: int):
     # fix SKU
     if 'sku' in part:
         new_sku = part.pop('sku')
-        #part['sku'] = {}
-        #part['sku'][source] = new_sku
+        # part['sku'] = {}
+        # part['sku'][source] = new_sku
     # fix links
     if 'links' in part:
         new_links = part.pop('links')
@@ -119,7 +119,8 @@ def adjust_structure(part: dict, source: str, ts: int):
         part['sku'][source] = {}
         part['sku'][source][new_sku[0]] = {}
         part['sku'][source][new_sku[0]]['links'] = new_links
-        part['sku'][source][new_sku[0]]['packaging'] = part.pop('packaging', None)
+        part['sku'][source][new_sku[0]]['packaging'] = part.pop(
+            'packaging', None)
 
     # generate IDs
     if 'mpn' in part and 'mfr' in part:
@@ -139,11 +140,11 @@ def adjust_structure(part: dict, source: str, ts: int):
     else:
         logging.debug("can't find MPN on part!")
         print("can't find MPN on part!")
-        return False 
+        return False
 
-    #adding timestamp ts of the normalization
+    # adding timestamp ts of the normalization
     part['ts_norm'] = c.now()
-    #adding timestamp ts of the crawler-
+    # adding timestamp ts of the crawler-
     part['ts_crawler'] = ts
     # last big modification of the structure of the json
     # we want to stuff everything but [mfr, mpn, category, sku, links,
@@ -162,10 +163,108 @@ def adjust_structure(part: dict, source: str, ts: int):
         'ts',
         'ts_norm',
         'ts_crawler',
-    		'mpn_raw']
+        'mpn_raw']
     part['properties'] = {}
     for k in list(part):
         if k not in main_keys:
-            #print("{0} not in main_keys".format(k))
+            # print("{0} not in main_keys".format(k))
             part['properties'][k] = part.pop(k, None)
+    return part
+
+
+def adjust_structure_minimal(part: dict, source: str, ts: int):
+
+    part = defaultdict(dict, part)
+    # fix mfr before everything
+    if 'mfr' in part:
+        # mfr = c.get_alias(part['mfr'])
+        mfr = c.get_mfr_mapping(part['mfr'])
+        part['mfr'] = mfr
+    # keep desciprtion
+    if 'description' in part:
+        raw_desc = deepcopy(part['description'])
+        part['description_raw'] = {}
+        part['description_raw'][source] = raw_desc
+        # fix category before normalization
+    if 'categories' in part:
+        raw_categories = deepcopy(part['categories'])
+        part['categories_raw'] = {}
+        part['categories_raw'][source] = raw_categories
+        # save raw mpn before normalizing
+    if 'mpn' in part:
+        raw_mpn = deepcopy(part['mpn'])
+        part['mpn_raw'] = {}
+        part['mpn_raw'][source] = raw_mpn
+    # remove availablity and pricing, minimum_quantity and packagecase
+    part.pop('availability', None)
+    part.pop('pricing', None)
+    part.pop('packagecase', None)
+    part.pop('minimum_quantity', None)
+    # fix lifecycle/life
+    if 'lifecycle' in part:
+        new_life = part.pop('lifecycle')
+        part['lifecycle'] = {}
+        part['lifecycle'][source] = new_life
+    # fix SKU
+    if 'sku' in part:
+        new_sku = part.pop('sku')
+        # part['sku'] = {}
+        # part['sku'][source] = new_sku
+    # fix links
+    if 'links' in part:
+        new_links = part.pop('links')
+        part['sku'] = {}
+        part['sku'][source] = {}
+        part['sku'][source][new_sku[0]] = {}
+        part['sku'][source][new_sku[0]]['links'] = new_links
+        part['sku'][source][new_sku[0]]['packaging'] = part.pop(
+            'packaging', None)
+
+    # generate IDs
+    if 'mpn' in part and 'mfr' in part:
+        part['mpn'] = re.sub('[^0-9a-zA-Z]+', '', part['mpn'])
+        id = (part['mpn'] + part['mfr']).lower().replace(" ", "")
+        hash_object = sha1(id.encode('utf-8'))
+        hex_dig = hash_object.hexdigest()
+        part['id'] = hex_dig
+    # print(part['id'])
+    elif 'mpn' in part:
+        part['mpn'] = re.sub('[^0-9a-zA-Z]+', '', part['mpn'])
+        id = part['mpn'].lower().replace(" ", "")
+        hash_object = sha1(id.encode('utf-8'))
+        hex_dig = hash_object.hexdigest()
+        part['id'] = hex_dig
+    # print(part['id'])
+    else:
+        logging.debug("can't find MPN on part!")
+        print("can't find MPN on part!")
+        return False
+
+    # adding timestamp ts of the normalization
+    part['ts_norm'] = c.now()
+    # adding timestamp ts of the crawler-
+    part['ts_crawler'] = ts
+    # last big modification of the structure of the json
+    # we want to stuff everything but [mfr, mpn, category, sku, links,
+    # description, lifecycle] as nested properties.
+
+
+    main_keys = [
+        'mfr',
+        'mpn',
+        'categories',
+        'categories_raw',
+        'sku',
+        'description',
+        'description_raw',
+        'lifecycle',
+        'properties',
+        'id',
+        'ts',
+        'ts_norm',
+        'ts_crawler',
+        'mpn_raw']
+    for k in list(part):
+        if k not in main_keys:
+            part.pop(k, None)
     return part
--- handler.py
+++ handler.py
@@ -6,12 +6,13 @@ import json
 import logging
 import os
 import sys
+import re
 from lambda_decorators import dump_json_body, json_http_resp
 import yaml
 import common as c
 import normalization as n
 
-from functions import adjust_structure
+from functions import adjust_structure, adjust_structure_minimal, MAPPING
 
 c.init()
 
@@ -41,7 +42,6 @@ def parse_record(rec):
 
 
 def norm_handler_kinesis(event: dict, *args):
-            # print(event)
     if 'Records' in event:
         for rec in event['Records']:
             message = parse_record(rec)
@@ -52,27 +52,53 @@ def norm_handler_kinesis(event: dict, *args):
         logging.error("no Records found in event")
         print(event)
 
-
     output = {'parts': []}
-
     if 'parts' in message:
         if 'source' in message:
             source = message['source']
             timestamp = message['ts']
         else:
-            logging.warning(
-                "could not find source (distributor) in message kinesis")
-            logging.warning(message)
-            #return False
+            logging.warning("could not find source (distributor) in message")
+            return False
         for part in message['parts']:
-            # this is where the magic happens
-            part = adjust_structure(part, source, timestamp)
-            output['parts'].append(part)
-            # if not rest_call:
-            #	print(c.publish_data(part))
-    #final_out =json.dumps(output)
+            part_properties = list(part.keys())
+            missing_keys = list(
+                map(lambda v: v in MAPPING[source], part_properties))
+            missing_keys = [not i for i in missing_keys]
+            missing_keys = [int(i) for i in missing_keys]
+            missing_keys = sum(missing_keys)
+            if missing_keys > 0:
+                missing_keys_list = []
+                for v in part.keys():
+                    if v not in MAPPING[source]:
+                        missing_keys_list.append(v)
+                logging.warning("there are {1} missing keys: {0}".format(missing_keys_list,missing_keys))
+            if 'categories' in part:
+                category = str(part['categories'])
+                category = re.sub('\', \'', '\',\'', category)
+                category_exists = '' 
+                if source != 'mouser':
+                    if category in n.categories[source]:
+                        category_exists = True
+                    elif category not in n.categories[source]:
+                        category_exists = False
+            else:
+                # failed to find category in part...
+                category_exists = False
+
+            # we only normlize and process digikey and mouser.
+            # we also only normlize categories we mapped and tested.
+            # # if we can't normalize it
+            if source not in MAPPING or missing_keys > 0 or category_exists is False:
+                # we can't handle it this part because it's from a distributor we don't know.
+                logging.warning("missing keys, mappings or new source. running minimal normalization")
+                part = adjust_structure_minimal(part, source, timestamp)
+                output['parts'].append(part)
+            elif source in MAPPING:
+                part = adjust_structure(part, source, timestamp)
+                output['parts'].append(part)
+
     c.publish_kinesis_single(output)
-    #print("length of payload: {}".format(len(output.encode("utf8"))))
     return True
 
 
@@ -91,24 +117,49 @@ def norm_handler_http(event, *args):
                 print(event['body'])
                 print("failed to load json from http request")
     output = {'parts': []}
-    #print(  message)
-    #print( 'parts' in message )
-    #print(  len(message.get('parts', 0)))   
-
     if 'parts' in message:
-        
+
         if 'source' in message:
             source = message['source']
             timestamp = message['ts']
         else:
             logging.warning("could not find source (distributor) in message")
-            #return False
+            return False
         for part in message['parts']:
-            # this is where the magic happens
-            #print("magic!")
-            #
-            # print(part.keys())
-            part = adjust_structure(part, source, timestamp)
-            output['parts'].append(part)
+            part_properties = list(part.keys())
+            missing_keys = list(
+                map(lambda v: v in MAPPING[source], part_properties))
+            missing_keys = [not i for i in missing_keys]
+            missing_keys = [int(i) for i in missing_keys]
+            missing_keys = sum(missing_keys)
+            if missing_keys > 0:
+                missing_keys_list = []
+                for v in part.keys():
+                    if v not in MAPPING[source]:
+                        missing_keys_list.append(v)
+                logging.warning("there are {1} missing keys: {0}".format(missing_keys_list,missing_keys))
+            if 'categories' in part:
+                category = str(part['categories'])
+                category = re.sub('\', \'', '\',\'', category)
+                category_exists = '' 
+                if source != 'mouser':
+                    if category in n.categories[source]:
+                        category_exists = True
+                    elif category not in n.categories[source]:
+                        category_exists = False
+            else:
+                # failed to find category in part...
+                category_exists = False
 
+            # we only normlize and process digikey and mouser.
+            # we also only normlize categories we mapped and tested.
+            # # if we can't normalize it
+            if source not in MAPPING or missing_keys > 0 or category_exists is False:
+                # we can't handle it this part because it's from a distributor we don't know.
+                logging.warning("missing keys, mappings or new source. running minimal normalization")
+                part = adjust_structure_minimal(part, source, timestamp)
+                output['parts'].append(part)
+            elif source in MAPPING:
+                part = adjust_structure(part, source, timestamp)
+                output['parts'].append(part)
     return output
