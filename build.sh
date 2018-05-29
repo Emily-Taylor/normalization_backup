be240ed48cfcc66f9f44af33c4fe411ac301522d
Ereli Eran <ereli@sourcingbot.com>
2018-05-29 10:34:04 -0400

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 git clone git@github.com:sourcingbot/common.git temp
-cp temp/array.json  normaliazation/mfr/array.json
+cp temp/array.json  normalization/mfr/array.json
 cp temp/mapping.yml mapping.yml
 cp temp/categories.yml  normalization/mapping.yml
 rm -rf temp/
