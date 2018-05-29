291571d832152586aff270e51253229d0be643a5
Ereli Eran <ereli@sourcingbot.com>
2018-05-29 14:45:48 -0400

    build script

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-set branch = $argv[1]
+if [ -n "$1" ]              # Tested variable is quoted.
+then
+ echo "branch is #1 is $1";  # Need quotes to escape #;
+fi 
 mkdir -p temp/
 cd temp/
-aws s3 cp s3://artifacts-1/common-artifacts-$branch.tar.gz .
-tar -xzvf common-artifacts-$branch.tar.gz
+aws s3 cp s3://artifacts-1/common-artifacts-$1.tar.gz .
+tar -xzvf common-artifacts-$1.tar.gz
 cp array.json  ../normalization/mfr/array.json
 cp mapping.yml ../mapping.yml
 cp categories.yml ../normalization/mapping.yml
 cd ..
 rm -rf temp/
+
+
+
+
