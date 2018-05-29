e8e8bf86996b50d6658310c9441b30fd1c3caa44
Ereli Eran <ereli@sourcingbot.com>
2018-05-29 14:26:56 -0400

    .

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set branch = $argv[1]
 mkdir -p temp/
 cd temp/
-aws s3 cp s3://artifacts-1/common-artifacts.tar.gz .
-tar -xzvf common-artifacts.tar.gz
+aws s3 cp s3://artifacts-1/common-artifacts-$branch.tar.gz .
+tar -xzvf common-artifacts-$branch.tar.gz
 cp array.json  ../normalization/mfr/array.json
 cp mapping.yml ../mapping.yml
 cp categories.yml ../normalization/mapping.yml
