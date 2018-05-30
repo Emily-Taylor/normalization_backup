83c3c1749a8d955c158aa333e594247ab69064aa
Ereli Eran <ereli@sourcingbot.com>
2018-05-30 10:53:37 -0400

    wrong filename!

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ aws s3 cp s3://artifacts-1/common-artifacts-$1.tar.gz .
 tar -xzvf common-artifacts-$1.tar.gz
 cp array.json  ../normalization/mfr/array.json
 cp mapping.yml ../mapping.yml
-cp categories.yml ../normalization/mapping.yml
+cp categories.yml ../normalization/categories.yml
 cd ..
 rm -rf temp/
 
