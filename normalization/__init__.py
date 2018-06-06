df68c93a7e6d34545bb475fd5cd76dbcb588f7df
rakshithindol <hindol@sourcingbot.com>
2018-06-06 16:25:46 +0200

    updated func parse_dimensions

--- normalization/__init__.py
+++ normalization/__init__.py
@@ -119,6 +119,7 @@ def extract_num(d: str) -> float:
             d = re.sub(r'\(.*\)', '', d)
             d = d.split(',', 1)[0]
             d = d.split('~', 1)[0]
+            d = d.split('/', 1)[0]
             
             
             if 'dBi @' in d:
@@ -378,22 +379,25 @@ def parse_dimensions(d: str):
     example: '0.276" L x 0.217" W (7.00mm x 5.50mm)'
     ignoring inches, focusing on millimeters
     """
-    regexp = re.compile(r'([\d\.]+mm)')
-    res = regexp.findall(d)
-    if len(res) == 2:
-        l, w = res[0], res[1]
-        l = float(Quantity(l, scale='mm'))
-        w = float(Quantity(w, scale='mm'))
-        return (l, w, np.nan)
-    elif len(res) == 3:
-        l, w, h = res[0], res[1], res[2]
-        l = float(Quantity(l, scale='mm'))
-        w = float(Quantity(w, scale='mm'))
-        h = float(Quantity(h, scale='mm'))
-        return (l, w, h)
-    elif len(res) == 1:
-        dim = float(Quantity(res[0], scale='mm'))
-        return (dim,)
+    if isinstance(d, str):
+        regexp = re.compile(r'([\d\.]+mm)')
+        res = regexp.findall(d)
+        if len(res) == 2:
+            l, w = res[0], res[1]
+            l = float(Quantity(l, scale='mm'))
+            w = float(Quantity(w, scale='mm'))
+            return (l, w, np.nan)
+        elif len(res) == 3:
+            l, w, h = res[0], res[1], res[2]
+            l = float(Quantity(l, scale='mm'))
+            w = float(Quantity(w, scale='mm'))
+            h = float(Quantity(h, scale='mm'))
+            return (l, w, h)
+        elif len(res) == 1:
+            dim = float(Quantity(res[0], scale='mm'))
+            return (dim, np.nan, np.nan)
+    else:
+        return (d, np.nan, np.nan)
 
 
 def split_band(d: str):
