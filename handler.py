84f557e65a1950636e7868456aaa394cffc105e3
rakshithindol <hindol@sourcingbot.com>
2018-08-14 16:14:07 +0200

    remove norm_handler_kinesis

--- handler.py
+++ handler.py
@@ -41,65 +41,65 @@ def parse_record(rec):
     return val
 
 
-def norm_handler_kinesis(event: dict, *args):
-    if 'Records' in event:
-        for rec in event['Records']:
-            message = parse_record(rec)
+#def norm_handler_kinesis(event: dict, *args):
+#    if 'Records' in event:
+#        for rec in event['Records']:
+#            message = parse_record(rec)
+#
+#    elif 'parts' in event:
+#        message = event
+#    else:
+#        logging.error("no Records found in event")
+#        print(event)
 
-    elif 'parts' in event:
-        message = event
-    else:
-        logging.error("no Records found in event")
-        print(event)
-
-    output = {'parts': []}
-    if 'parts' in message:
-        if 'source' in message:
-            source = message['source']
-            timestamp = message['ts']
-        else:
-            logging.warning("could not find source (distributor) in message")
-            return False
-        for part in message['parts']:
-            part_properties = list(part.keys())
-            missing_keys = list(
-                map(lambda v: v in MAPPING[source], part_properties))
-            missing_keys = [not i for i in missing_keys]
-            missing_keys = [int(i) for i in missing_keys]
-            missing_keys = sum(missing_keys)
-            if missing_keys > 0:
-                missing_keys_list = []
-                for v in part.keys():
-                    if v not in MAPPING[source]:
-                        missing_keys_list.append(v)
-                logging.warning("there are {1} missing keys: {0}".format(missing_keys_list,missing_keys))
-            if 'categories' in part:
-                category = str(part['categories'])
-                category = re.sub('\', \'', '\',\'', category)
-                category_exists = '' 
-                if source != 'mouser':
-                    if category in n.categories[source]:
-                        category_exists = True
-                    elif category not in n.categories[source]:
-                        category_exists = False
-            else:
-                # failed to find category in part...
-                category_exists = False
-
-            # we only normlize and process digikey and mouser.
-            # we also only normlize categories we mapped and tested.
-            # # if we can't normalize it
-            if source not in MAPPING or missing_keys > 0 or category_exists is False:
-                # we can't handle it this part because it's from a distributor we don't know.
-                logging.warning("missing keys, mappings or new source. running minimal normalization")
-                part = adjust_structure_minimal(part, source, timestamp)
-                output['parts'].append(part)
-            elif source in MAPPING:
-                part = adjust_structure(part, source, timestamp)
-                output['parts'].append(part)
-
-    c.publish_kinesis_single(output)
-    return True
+#    output = {'parts': []}
+#    if 'parts' in message:
+#        if 'source' in message:
+#            source = message['source']
+#            timestamp = message['ts']
+#        else:
+#            logging.warning("could not find source (distributor) in message")
+#            return False
+#        for part in message['parts']:
+#            part_properties = list(part.keys())
+#            missing_keys = list(
+#                map(lambda v: v in MAPPING[source], part_properties))
+#            missing_keys = [not i for i in missing_keys]
+#            missing_keys = [int(i) for i in missing_keys]
+#            missing_keys = sum(missing_keys)
+#            if missing_keys > 0:
+#                missing_keys_list = []
+#                for v in part.keys():
+#                    if v not in MAPPING[source]:
+#                        missing_keys_list.append(v)
+#                logging.warning("there are {1} missing keys: {0}".format(missing_keys_list,missing_keys))
+#            if 'categories' in part:
+#                category = str(part['categories'])
+#                category = re.sub('\', \'', '\',\'', category)
+#                category_exists = '' 
+#                if source != 'mouser':
+#                    if category in n.categories[source]:
+#                        category_exists = True
+#                    elif category not in n.categories[source]:
+#                        category_exists = False
+#            else:
+#                # failed to find category in part...
+#                category_exists = False
+#
+#            # we only normlize and process digikey and mouser.
+#            # we also only normlize categories we mapped and tested.
+#            # # if we can't normalize it
+#            if source not in MAPPING or missing_keys > 0 or category_exists is False:
+#                # we can't handle it this part because it's from a distributor we don't know.
+#                logging.warning("missing keys, mappings or new source. running minimal normalization")
+#                part = adjust_structure_minimal(part, source, timestamp)
+#                output['parts'].append(part)
+#            elif source in MAPPING:
+#                part = adjust_structure(part, source, timestamp)
+#                output['parts'].append(part)
+#
+#    c.publish_kinesis_single(output)
+#    return True
 
 
 @json_http_resp
